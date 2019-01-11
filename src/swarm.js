a5eb3738de3baea6a42a74eef8dc1f19ed00a0ce
M Yahya <ya7yaz@gmail.com>
2019-01-11 13:11:26 +0200

    essential swarm functions

---
+++ src/swarm.js
@@ -0,0 +1,75 @@
+'use strict'
+const { exec } = require('child_process')
+
+class Swarm {
+  constructor (opts) {
+    this._defaults = {
+      driver: 'google',
+      zone: 'us-east1-b',
+      machineType: 'n1-standard-1',
+      tags: 'swarm-cluster',
+      projectId: 'test-harness-226018'
+    }
+  }
+
+  createMachine (opts, cb) {
+    let driver = opts.driver || this._defaults.driver
+    // TODO sanitize user opts here
+    exec(`docker-machine create ${opts.name} \
+      --driver ${driver} \
+      --${driver}-zone ${opts.zone || this._defaults.zone} \
+      --${driver}-machine-type ${opts.machineType || this.defaults.machineType} \
+      --${driver}-tags ${opts.tags || this._defaults.tags} \
+      --${driver}-project ${this._defaults.projectId}`, cb)
+  }
+
+  scp (origin, destination, opts, cb) {
+    exec(`docker-machine scp ${origin} ${destination}`, cb)
+  }
+
+  getPubIP (machineName, cb) {
+    exec(`docker-machine ip ${machineName}`, cb)
+  }
+
+  setEnv (machineName, cb) {
+    exec(`docker-machine env ${machineName}`, cb)
+  }
+
+  unsetEnv (cb) {
+    exec(`docker-machine env --unset`, cb)
+  }
+
+  init (managerName, cb) {
+    this.setEnv(managerName, (err) => {
+      if (err) throw err
+      exec(`docker swarm init`, cb)
+    })
+  }
+
+  join (machineName, managerIP, cb) {
+    this.setEnv(machineName, (err) => {
+      if (err) throw err
+      // TODO get the managers internal IP automatically.
+      exec(`docker swarm join ${managerIP}:2377`)
+    })
+  }
+
+  deployComposeFile (filePath, prefix, managerName, cb) {
+    this.setEnv(managerName, (err) => {
+      exec(`docker stack deploy --composefile ${filePath} ${prefix}`, cb)
+    })
+  }
+
+  stopStack (prefix, cb) {
+    this.setEnv(managerName, (err) => {
+      exec(`docker stack rm ${prefix}`, cb)
+    })
+  }
+
+  tearDown (machineName, cb) {
+    exec(`docker-machine rm ${machineName}`, cb)
+  }
+
+}
+
+module.exports = Swarm
