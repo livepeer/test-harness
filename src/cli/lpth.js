2893198ca8d008b81850ebaadceb22a87451a621
M Yahya <ya7yaz@gmail.com>
2019-01-28 00:30:29 +0200

    port cli

--- src/cli/lpth.js
+++ src/cli/lpth.js
@@ -5,6 +5,7 @@ const fs = require('fs')
 const path = require('path')
 const NetworkCreator = require('../networkcreator')
 const dockercompose = require('docker-compose')
+const YAML = require('yaml')
 
 function parsePath (val) {
   console.log(`parsing ${path.resolve(val)} config:`)
@@ -40,4 +41,80 @@ program
       })
     })
   })
+
+program
+  .command('port [name] [lpnode]')
+  .description('get forwarded port for a livepeer node')
+  .option('-t, --type [type]', 'which port (cli, rtmp, http)?')
+  .action((name, lpnode, env) => {
+    // console.log(name, lpnode, env.type)
+    parseDockerCompose(name, (err, experiment) => {
+      if (err) throw err
+      let servicesNames = Object.keys(experiment.services)
+      let num = lpnode.split('_')[1]
+      let port = null
+      let serviceName = null
+      if (lpnode.startsWith('b_')) {
+        serviceName = `broadcaster_${num}`
+      } else if (lpnode.startsWith('b_')) {
+        serviceName = `transcoder_${num}`
+      } else if (lpnode.startsWith('o_')) {
+        serviceName = `orchestrator_${num}`
+      } else {
+        serviceName = lpnode
+      }
+      // console.log('service name: ', serviceName)
+      if (experiment.services[serviceName]) {
+        switch (env.type) {
+          case 'cli':
+            port = getForwardedPort(experiment, serviceName, '7935')
+            break
+          case 'rtmp':
+            port = getForwardedPort(experiment, serviceName, '1935')
+            break
+          case 'http':
+            port = getForwardedPort(experiment, serviceName, '8935')
+            break
+          default:
+        }
+
+        console.log(port)
+      } else {
+        // console.log(`\n`)
+      }
+      process.exit()
+    })
+  })
+
+function parseDockerCompose (name, cb) {
+  let parsedCompose = null
+  fs.readFile(path.resolve(__dirname, `../../dist/${name}/docker-compose.yml`), 'utf-8', (err, _file) => {
+    if (err) throw err
+    try {
+      parsedCompose = YAML.parse(_file)
+    } catch (e) {
+      throw e
+    }
+
+    cb(null, parsedCompose)
+  })
+}
+
+function getForwardedPort (parsedCompose, service, origin) {
+  if (!parsedCompose.services[service]) {
+    throw new Error(`container ${service} isn't in the compose file`)
+  }
+
+  let portPair = parsedCompose.services[service].ports.filter((port) => {
+    return (port.match(new RegExp(`:${origin}`, 'g')))
+  })
+
+  if (portPair.length > 0) {
+    return parseInt(portPair[0].match(/(.*)?:/g)[0].slice(0, -1))
+  } else {
+    return null
+  }
+}
+
+
 program.parse(process.argv)
