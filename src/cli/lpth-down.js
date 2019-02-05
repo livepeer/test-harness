cf147662b2a6d501eb8ef27b84a20b6634e32862
Ivan Tivonenko <darkdarkdragon@gmail.com>
2019-02-05 21:25:41 +0200

    fix for `down` command

--- src/cli/lpth-down.js
+++ src/cli/lpth-down.js
@@ -1,27 +1,27 @@
 #!/usr/bin/env node
 
 const program = require('commander')
+const path = require('path')
 const { parseConfigFromCommandLine } = require('./helpers.js')
-// const { prettyPrintDeploymentInfo } = require('../helpers')
 const Swarm = require('../swarm')
 const dockercompose = require('docker-compose')
 
 async function run(parsedCompose) {
   const name = parsedCompose.configName
   if (parsedCompose.isLocal) {
-      console.log('Running docker-compose down...')
-      const logs = await dockercompose.execCompose('down', ['-v'], {
-        cwd: path.resolve(__dirname, `../../dist/${name}/`),
-        logs: true,
-      })
-      console.log(logs)
-      console.log(`experiment ${name} services stopped.`)
-      process.exit(0)
+    console.log('Running docker-compose down...')
+    const logs = await dockercompose.execCompose('down', ['-v'], {
+      cwd: path.resolve(__dirname, `../../dist/${name}/`),
+      logs: true,
+    })
+    console.log(logs)
+    console.log(`experiment ${name} services stopped.`)
+    process.exit(0)
   } else {
-      const swarm = new Swarm(name)
-      console.log(`Removing VM instances for ${name}`)
-      await swarm.tearDown(name)
-      console.log(`experiment ${name} VM instances removed.`)
+    const swarm = new Swarm(name)
+    console.log(`Removing VM instances for ${name}`)
+    await swarm.tearDown(name)
+    console.log(`experiment ${name} VM instances removed.`)
   }
 }
 
