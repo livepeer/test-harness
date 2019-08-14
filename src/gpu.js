7156de0bd05b702da3a71d0c22965fbad0dc397c
Yahya <ya7yaz@gmail.com>
2019-08-14 10:59:02 +0200

    add -y

--- src/gpu.js
+++ src/gpu.js
@@ -1,7 +1,7 @@
 'use strict'
 
+const path = require('path')
 const utils = require('./utils/helpers')
-
 
 /**
  * installs openvpn on a given gcp machine.
@@ -10,12 +10,12 @@ const utils = require('./utils/helpers')
  */
 async function installOpenvpn (machine, zone) {
     return utils.remotelyExec(machine, zone, 
-        `sudo apt-get install openvpn`)
+        `sudo apt-get -y install openvpn`)
 }
 
 
-async function uploadOvpnFile (path, targetPath) {
-    return utils.scp(path, targetPath)
+async function uploadOvpnFile (sourcePath, targetPath) {
+    return utils.scp(path.resolve(__dirname, sourcePath), targetPath)
 }
 
 async function runOpenvpn (machine, zone) {
